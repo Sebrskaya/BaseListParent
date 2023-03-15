dccb58958c970208e419828592114a26f4e2f639
Sebrskaya <114391597+Sebrskaya@users.noreply.github.com>
2023-03-15 11:16:09 +0500

    Update Test.cs

--- BaseListParent/Test.cs
+++ BaseListParent/Test.cs
@@ -1,12 +1,12 @@
-﻿
-BaseList ChainList = new ChainList.Methods();
+
+BaseList ChainList = new ChainList();
 BaseList ArrayList = new ArrayList();
 BaseList Brat = new ArrayList();
 Random random = new Random();
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < 10000; i++)
 {
-    int method = random.Next(0, 0);
+    int method = random.Next(0, 5);
     int pos = random.Next(0, 10001);
     int data = random.Next(0, 100);
 
@@ -17,44 +17,60 @@ for (int i = 0; i < 10; i++)
             ArrayList.Add(data);
             break;
 
-        case 11:
+        case 1:
             ChainList.Insert(pos, data);
             ArrayList.Insert(pos, data);
             break;
 
-        case 21:
+        case 2:
             ChainList.Clear();
             ArrayList.Clear();
             break;
 
-        case 31:
+        case 3:
             ChainList.Delete(pos);
             ArrayList.Delete(pos);
             break;
 
-        case 41:
+        case 4:
             ChainList[pos] = data;
             ArrayList[pos] = data;
+            break;
+        
+        case 5:
+            ChainList.Sort();
+            ArrayList.Sort();
             break;
     }
     if (ArrayList.Lenght() != ChainList.Lenght())
     {
         //Console.WriteLine(i + " Count слетел!");
     }
+    //Check Assign \/
+    ArrayList.Assign(ChainList);
+    if (! ArrayList.EqualsTo(ChainList))
+        Console.WriteLine("Assign не работает");
+    ChainList.Assign(ArrayList);
+    if (! ChainList.EqualsTo(ArrayList))
+        Console.WriteLine("Assign не работает");
+
+    //Check Clone
+    Brat = ArrayList.Clone();
+    if (ArrayList.EqualsTo(Brat) == false)
+        Console.WriteLine("Clone не работает");
+    Brat = ChainList.Clone();
+    if (ArrayList.EqualsTo(Brat) == false)
+        Console.WriteLine("Clone не работает");
+
+    //Сравнение листов
+    if (ArrayList.EqualsTo(ChainList) == false)
+        Console.WriteLine("Ar и Ch Листы не равны");
+    if (ArrayList.EqualsTo(Brat) == false)
+        Console.WriteLine("Ar и Br Листы не равны");
+    if (ArrayList.EqualsTo(Brat) == false)
+        Console.WriteLine("Ch и Br Листы не равны");
 }
-for (int i = 0; i < ChainList.Lenght(); i++)
-{
-    if (ArrayList[i] != ChainList[i])
-    {
-        Console.WriteLine(i + " Элементы не ровны!");
-    }
-}
-ChainList.Sort();
-ArrayList.Sort();
-ArrayList.Clone();
-ChainList.Assign(ArrayList);
+
 ChainList.Print();
 ArrayList.Print();
-Brat.Print();
-Brat = ArrayList.Clone();
 Brat.Print();
